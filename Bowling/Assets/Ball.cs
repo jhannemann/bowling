df324ee6587c5d8077226a7c11bee613d951f615
Jens Hannemann <j.hannemann@ieee.org>
2018-05-17 16:12:13 -0400

    Add script to shoot the ball
    The ball is now a prefab and has a script attached that allows us to
    shoot the ball by hitting the space bar.

---
+++ Bowling/Assets/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 10;
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rigidBody.velocity = new Vector3 (0, 0, speed);
+		}
+	}
+}
